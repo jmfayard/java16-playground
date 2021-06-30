7e6089c390d0a470005aeced9dfbd3880ed26cd2
Jean-Michel Fayard <jmfayard@gmail.com>
2021-06-30 10:22:27 +0200

    Use Gradle java toolchains
    https://blog.gradle.org/java-toolchains
    https://docs.gradle.org/current/userguide/toolchains.html

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,12 @@ plugins {
     application
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(16))
+    }
+}
+
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
